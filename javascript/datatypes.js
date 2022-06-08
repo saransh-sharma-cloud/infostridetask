48be128d4ef5c0296e2a47076e1a8910544fb0ec
saransh-sharma-cloud <saranshsharma723@gmail.com>
2022-06-09 01:58:28 +0530

    all work done

---
+++ javascript/datatypes.js
@@ -0,0 +1,16 @@
+let name = "saransh";
+console.log("my string is " +   name);
+
+console.log("Data type is " +   (typeof name));
+
+let marks =34;
+console.log("Data type is " +   (typeof marks));
+
+let nullVar = null;
+console.log("Data type is " +   (typeof nullVar));
+
+let undef = undefined;
+console.log("Data type is " +   (typeof undef));
+
+myarr = [1,2,3,4,false,"string"];
+console.log("Data type is " +   (typeof myarr));
---
+++ javascript/tut3.js
@@ -0,0 +1,27 @@
+console.log('tut3')
+var name = 'saransh';
+var channel;
+var marks = 34;
+
+console.log(name, channel, marks)
+  
+
+var city ="mohali"
+
+console.log(city);
+ 
+const ownerName =" elon musk";
+
+console.log(ownerName);
+
+{
+    let city = "chd";
+    city = "panchkula";
+    console.log(city);
+
+}
+console.log(city);
+
+ const arr1 = [12,32,34,65,3];
+ arr1.push(43);
+ console.log(arr1)
